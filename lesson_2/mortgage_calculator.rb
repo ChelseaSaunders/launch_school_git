bb33b1de76219287fc3c782e67a2f1a18863c67d
Chelsea Saunders <c_chilcoat@msn.com>
2020-09-16 21:59:33 +0000

    Updated Mortgage Calculator assignment after code review.

--- lesson_2/mortgage_calculator.rb
+++ lesson_2/mortgage_calculator.rb
@@ -13,31 +13,122 @@ def float?(num)
   num == num.to_f.to_s
 end
 
-def number?(num)
-  num.to_i > 0 && (integer?(num) || float?(num))
+def positive_number?(num)
+  num.to_f > 0 && (integer?(num) || float?(num))
 end
 
-def get_amounts
+def zero?(num)
+  num.to_f == 0 && (num.to_f.to_s == num || num.to_i.to_s == num)
+end
+
+def positive_integer?(num)
+  integer?(num) && positive_number?(num)
+end
+
+def get_loan_amount
+  prompt(MESSAGES['loan_amount_prompt'])
   amount = ''
   loop do
     amount = gets.chomp
-    if number?(amount)
+    if positive_number?(amount)
       break
     else
-      prompt(MESSAGES['invalid_input'])
+      prompt(MESSAGES['invalid_input_loan'])
     end
   end
   amount
 end
 
+def get_apr_amount
+  prompt(MESSAGES['apr_prompt'])
+  amount = ''
+  loop do
+    amount = gets.chomp
+    if positive_number?(amount) || zero?(amount)
+      break
+    else
+      prompt(MESSAGES['invalid_input_apr'])
+    end
+  end
+  amount
+end
+
+def apr_mpr(num)
+  mpr = ''
+  if num.to_f == 0
+    mpr = 0
+  else
+    mpr = (num.to_f / 12) / 100
+  end
+  mpr
+end
+
+def get_year_amount
+  prompt(MESSAGES['years_prompt'])
+  amount = ''
+  loop do
+    amount = gets.chomp
+    if positive_integer?(amount) || zero?(amount)
+      break
+    else
+      prompt(MESSAGES['invalid_input_years'])
+    end
+  end
+  amount
+end
+
+def get_month_amount
+  prompt(MESSAGES['months_prompt'])
+  amount = ''
+  loop do
+    amount = gets.chomp
+    if positive_integer?(amount) == false && zero?(amount) == false
+      prompt(MESSAGES['invalid_input_months'])
+    elsif amount.to_i < 0
+      prompt(MESSAGES['invalid_input_months'])
+    elsif amount.to_i > 11
+      prompt(MESSAGES['invalid_input_months'])
+    else
+      break
+    end
+  end
+  amount
+end
+
+def calculate_payment(loan, interest, months)
+  if interest == 0
+    monthly_payment = loan.to_f / months.to_f
+  else
+    monthly_payment = loan.to_f * (interest /
+    (1 - (1 + interest)**(-months.to_f)))
+  end
+  monthly_payment
+end
+
+def another_calculation
+  prompt(MESSAGES['another_calculation'])
+  start_over = ''
+  loop do
+    answer = gets.chomp.downcase
+    if answer.downcase == 'y' || answer.downcase == 'yes'
+      start_over = true
+      break
+    elsif answer.downcase == 'n' || answer.downcase == 'no'
+      start_over = false
+      break
+    else
+      prompt(MESSAGES['invalid_input_another_calculation'])
+    end
+  end
+  start_over
+end
+
 prompt(MESSAGES['welcome'])
 
 loop do
-  prompt(MESSAGES['loan_amount_prompt'])
-  loan_amount = get_amounts
+  loan_amount = get_loan_amount
 
-  prompt(MESSAGES['apr_prompt'])
-  apr = get_amounts
+  apr = get_apr_amount
 
   monthly_interest = (apr.to_f / 12) / 100
 
@@ -46,20 +137,12 @@ loop do
 
   loop do
     prompt(MESSAGES['loan_duration_prompt'])
-    prompt(MESSAGES['years_prompt'])
-    years = get_amounts
-
-    prompt(MESSAGES['months_prompt'])
-    months = get_amounts
+    years = get_year_amount
+    months = get_month_amount
 
     months = (years.to_i * 12) + months.to_i
 
     prompt("Is the total loan duration #{months} months? (Y for yes)")
-
-    # Per some discussion in the forum around trying to do string interpolation
-    # with yaml being a bit more complex than necessary at our current learning
-    # stage, I decided to skip the yaml for the two strings that use
-    # interpolation in this assignment.
 
     answer = gets.chomp.downcase
     if answer == 'y'
@@ -69,15 +152,11 @@ loop do
     end
   end
 
-  monthly_payment = loan_amount.to_f * (monthly_interest /
-                    (1 - (1 + monthly_interest)**(-months.to_f)))
+  monthly_payment = calculate_payment(loan_amount, monthly_interest, months)
 
   prompt("Your monthly payment is $#{monthly_payment.round(2)}.")
 
-  # I just used the Float.round method because I can't seem to understand the
-  # Kernel.format method yet. I'm trying to get my head around it, though!
-
-  prompt(MESSAGES['another_calculation'])
-  answer = gets.chomp.downcase
-  break if answer != 'y'
+  break if another_calculation == false
 end
+
+prompt(MESSAGES['exit_thank_you'])
